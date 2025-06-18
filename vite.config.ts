9a2f90597b3805f35c8db707b200c9afe5659617
roobeen-b <rubinbaidhya@gmail.com>
2025-06-18 15:36:01 +0545

    Added alias for imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@styles": path.resolve(__dirname, "./src/_style"),
+      "@config": path.resolve(__dirname, "./src/config"),
+      "@utils": path.resolve(__dirname, "./src/_utils"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@lang": path.resolve(__dirname, "./src/lang"),
+      "@api": path.resolve(__dirname, "./src/api"),
+      "@store": path.resolve(__dirname, "./src/store"),
+    },
+  },
+});
